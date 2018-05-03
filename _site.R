21c9a086eccbf317946882a8e46857b593b9d6d3
jules32 <jstewart@nceas.ucsb.edu>
2018-05-03 15:03:18 -0700

    with @eschemmel, update website with just layers page

--- _site.R
+++ _site.R
@@ -25,11 +25,30 @@ if (RCurl::url.exists(layers_csv)) layers <- readr::read_csv(layers_csv)
 if (RCurl::url.exists(conf_csv))   weight <- readr::read_csv(conf_csv) %>%
   select(goal, weight)
 
-## save local copy of conf/goals.Rmd
-#conf_goals_rmd <- file.path(dir_scenario_gh, 'conf/web/goals.Rmd')
+## save local copies of Rmds to knit-child ----
 
-#if (RCurl::url.exists(scores_csv)) {
-#  conf_goals <- readr::read_lines(conf_goals_rmd)
-#  readr::write_lines(conf_goals, path = 'conf_goals.Rmd', append = FALSE)
-#}
+dir_raw_draft <- 'https://raw.githubusercontent.com/OHI-Science/whi/master'
 
+to_copy <- c(# 'region2017/conf/web/goals.Rmd',
+             'region2017/conf/web/layers_all.Rmd')
+             #'region2017/conf/web/layers_table.Rmd',
+             #'region2017/conf/web/OHI_Hawaii.bib')
+
+for (f in to_copy) { # f <-  'region2017/conf/web/layers_all.Rmd'
+
+  fp <- file.path(dir_raw_draft, f)
+
+  ## if the url exists, save a copy.
+  if (RCurl::url.exists(fp)) {
+    f_web   <- readr::read_lines(fp)
+    if ( tools::file_ext(fp) == 'Rmd' ) {
+      f_local <- paste0('local_', basename(fp))
+    } else {
+      f_local <- basename(fp)
+    }
+    readr::write_lines(f_web, path = f_local, append = FALSE)
+    message(sprintf('saving %s', f_local))
+  } else {
+    message(sprintf('%s does not exist', fp))
+  }
+}
